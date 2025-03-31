93465f6788a2cd8715876f1959269b0f43ca0819
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-31 22:12:52 +0800

    finished the convert to upper program

--- convert_to_upper_case_without_using_upper.py
+++ convert_to_upper_case_without_using_upper.py
@@ -1,4 +1,7 @@
 # enter a word
-word= input('Please enter a word: ')
+word = input('Please enter a word: ')
 # translate the word to it's upper case counter part
+reference = str.maketrans("abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+upper_cased = word.translate(reference)
 # print the upper cased word
+print (upper_cased)
