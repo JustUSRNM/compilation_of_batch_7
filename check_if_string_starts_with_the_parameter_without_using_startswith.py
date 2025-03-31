eaf67ef3302333644189d074d5f63cf422381de0
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-31 22:31:25 +0800

    finished the check if starts with program; finished the first half of batch 7

--- check_if_string_starts_with_the_parameter_without_using_startswith.py
+++ check_if_string_starts_with_the_parameter_without_using_startswith.py
@@ -1,7 +1,7 @@
 # input a word and the parameter to be used
-word= input('Please enter a word: ')
-parameter= input('Please enter a parameter: ')
+word = input('Please enter a word: ')
+parameter = input('Please enter a parameter: ')
 # using indexing compare the parameter and word
-endswith= word[-len(parameter):] == parameter
+startswith = word[:len(parameter)] == parameter
 # print the boolean
-print (endswith)
+print (startswith)
