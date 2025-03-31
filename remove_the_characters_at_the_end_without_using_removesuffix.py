8ed40d127ff4824f6e009668ffea8039a3fa932b
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-31 22:07:40 +0800

    finished the remove suffix program

--- remove_the_characters_at_the_end_without_using_removesuffix.py
+++ remove_the_characters_at_the_end_without_using_removesuffix.py
@@ -1,9 +1,9 @@
 # enter a word and the suffix to be removed
-word= input('Please enter a word: ')
-parameter= input('Please enter a prefix that is in the word: ')
+word = input('Please enter a word: ')
+parameter = input('Please enter a prefix that is in the word: ')
 # check if the suffix is in the word
-if word.startswith(parameter):
+if word.endswith(parameter):
 # get the len of the suffix and remove it to the word
-    no_prefix= word[len(parameter):]
+    no_suffix = word[:-len(parameter)]
 # print the word without the suffix
-    print (no_prefix)
+    print (no_suffix)
