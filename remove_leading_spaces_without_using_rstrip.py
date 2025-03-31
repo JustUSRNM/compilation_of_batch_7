8482bc9688bc01a1a207919e9aa312ae335f8633
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-31 22:02:53 +0800

    finished the remove leading spaces program

--- remove_leading_spaces_without_using_rstrip.py
+++ remove_leading_spaces_without_using_rstrip.py
@@ -1,10 +1,10 @@
 # input a word with leading spaces
-word= input('Please enter a word with mutiple starting spaces:')
+word = input('Please enter a word with mutiple leading spaces:')
 # count number of spaces at the end
-number_of_spaces= 0
+number_of_spaces = -1
 while word[number_of_spaces] == " ":
-    number_of_spaces += 1
+    number_of_spaces -= 1
 # remove the spaces
-word_no_spaces= word[number_of_spaces:]
+word_no_spaces = word[:number_of_spaces+1]
 #print the word without the spaces
 print (word_no_spaces)
