672f5befcacca9120d24561d3bdb58f2d4414ed4
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-01 19:33:23 +0800

    finished the count program

--- count_how_many_times_the_parameter_appear_without_using_count.py
+++ count_how_many_times_the_parameter_appear_without_using_count.py
@@ -1,5 +1,14 @@
 # input a word and the parameter
-word = int(input('Please enter a word: '))
-parameter = int(input('Enter the character or substring to be counted: '))
+word = (input('Please enter a word: '))
+parameter = (input('Enter the character or substring to be counted: '))
 # using while loop, count the parameter in the word
+count = 0
+position = 0
+while position <= len(word) - len(parameter):
+    if word[position:position + len(parameter)] == parameter:
+        count += 1
+        position += len(parameter)
+    else:
+        position += 1
 # print how many times the parameter appeared
+print (count)
