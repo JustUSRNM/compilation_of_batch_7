6d40f0427c86849bcfee786c0f1ee7a2841c144f
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-01 19:45:17 +0800

    finished the rindex program; finished batch 7

--- print_the_first_location_of_the_parameter_starting_at_the_right.py
+++ print_the_first_location_of_the_parameter_starting_at_the_right.py
@@ -1,5 +1,9 @@
 # input a word and the parameter to find
-word = int(input('Please enter a word: '))
-parameter = int(input('Enter the character or substring to be searched: '))
+word = (input('Please enter a word: '))
+parameter = (input('Enter the character or substring to be searched: '))
 # using for loop, search the rightmost parameter then break when the parameter is found
+for position in range(len(word) - len(parameter), -1, -1):
+    if word[position:position + len(parameter)] == parameter:
+        break
 # print the postion of the parameter
+print (position)
