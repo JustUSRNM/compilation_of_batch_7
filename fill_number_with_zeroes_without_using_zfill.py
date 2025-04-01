f6b9b8ac2c8bbf3a5bc42a28fe7b290981bf4d0b
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-01 19:27:01 +0800

    finished the zfill program

--- fill_number_with_zeroes_without_using_zfill.py
+++ fill_number_with_zeroes_without_using_zfill.py
@@ -2,5 +2,8 @@
 number = int(input('Please enter a number: '))
 parameter = int(input('Enter how many digit: '))
 # covert the number to a string
+string_number = str(number) 
 # subtract the parameter to the length of the number to get how many zeroes are added
+zeroes = parameter - len(string_number)
 # print the number with the zeroes
+print (zeroes*"0" + string_number)
