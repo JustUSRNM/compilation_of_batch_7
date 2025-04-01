5afea30a719f8d9147b6ee5291ddc529f9b91597
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-01 19:23:22 +0800

    finished the right justify program

--- right_justify_without_using_rjust.py
+++ right_justify_without_using_rjust.py
@@ -1,10 +1,10 @@
 # input a word and the width parameter of rjust
-word= input('Please enter a word: ')
-parameter= int(input('Enter what width the word be in: '))
+word = input('Please enter a word: ')
+parameter = int(input('Enter what width the word be in: '))
 # the difference between the width and the word is the amount of spaces added
 if parameter > len(word):
     spaces= parameter - len(word)
 elif parameter <= len(word):
     spaces = 0
 # print the word that justified to the right
-print (word+ spaces*" ")
+print ( spaces*" "+word)
